13131fc7e781f99a9f139b33fc5562ae32637522
AyrtonPereira <ayrton_pereira@live.com>
2023-02-05 23:15:16 -0300

    feat: created basic form components

---
+++ src/components/form/BaseButton.vue
@@ -0,0 +1,40 @@
+<script setup>
+const props = defineProps({
+  type: { type: String, default: "primary" },
+  label: { type: String },
+  loading: { type: Boolean, default: false },
+  blocked: { type: Boolean, default: false },
+  disabled: { type: Boolean, default: false },
+});
+
+const buttonPills = {
+  primary: "cuco-color-primary text-white border-transparent",
+  secondary: "cuco-color-secondary border-transparent background-transparent",
+  danger:
+    "cuco-color-danger text-white border-transparent focus:border-none focus:outline-none",
+};
+</script>
+<template>
+  <button
+    :class="`transition-all duration-200 flex w-full justify-center rounded-md border py-2 px-4 text-sm font-medium focus:outline-none focus:ring-2  focus:ring-offset-2 ${
+      buttonPills[type]
+    }
+    ${loading ? 'opacity-20 animate-pulse' : ''} 
+    ${blocked ? 'pointer-events-none opacity-20' : ''}`"
+  >
+    <slot>{{ label }}</slot>
+  </button>
+</template>
+<style lang="scss" scoped>
+.cuco-color-danger {
+  background-color: #ee2279;
+}
+
+.cuco-color-secondary {
+  color: #969cb2;
+}
+
+.cuco-color-primary {
+  background-color: #12a454;
+}
+</style>
---
+++ src/components/form/BaseInput.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { Field } from "vee-validate";
+import { computed } from "vue";
+
+const props = defineProps(["modelValue"]);
+const emit = defineEmits(["update:modelValue"]);
+
+const value = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(value) {
+    emit("update:modelValue", value);
+  },
+});
+</script>
+<template>
+  <Field
+    v-model="value"
+    class="rounded-md relative block w-full appearance-none border border-zinc-300 px-3 py-2 text-zinc-900 placeholder-zinc-500 focus:z-10 focus:border-zinc-500 focus:outline-none focus:ring-zinc-500 sm:text-sm"
+  />
+</template>
