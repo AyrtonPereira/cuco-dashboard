6e32653579f410339eb1600e89519e77c3849624
AyrtonPereira <ayrton_pereira@live.com>
2023-02-07 15:21:02 -0300

    chore: updated application imports

--- src/main.js
+++ src/main.js
@@ -1,14 +1,35 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp, markRaw } from "vue";
+import { createPinia } from "pinia";
+import Toast from "vue-toastification";
+import toastPlugin from "./plugins/toastPlugin";
+import "vue-toastification/dist/index.css";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/main.css'
+/*import of most used components*/
+import BaseButton from "@/components/form/BaseButton.vue";
+import BaseInput from "@/components/form/BaseInput.vue";
+import piniaPluginPersistedState from "pinia-plugin-persistedstate";
+/*******************************/
 
-const app = createApp(App)
+import "./assets/main.css";
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
+export const pinia = createPinia();
+pinia.use(piniaPluginPersistedState).use(({ store }) => {
+  store.router = markRaw(router);
+});
 
-app.mount('#app')
+app
+  .use(Toast, {
+    toastClassName: "panel-toast",
+    transition: "Vue-Toastification__fade",
+  })
+  .use(toastPlugin)
+  .use(pinia)
+  .use(router)
+  .component("BaseButton", BaseButton)
+  .component("BaseInput", BaseInput);
+
+app.mount("#app");
