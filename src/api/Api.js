7f0f769bccfb8082073732830132cf5987073dee
Ayrton Pereira <39170090+AyrtonPereira@users.noreply.github.com>
2023-02-07 19:36:35 -0300

    Update Api.js

--- src/api/Api.js
+++ src/api/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class API {
   constructor(options = {}) {
     this.api = axios.create({
-      baseURL: "http://localhost:3003",
+      baseURL: "https://jsonplaceholder.typicode.com/db",
       timeout: 3000,
       ...options,
     });
