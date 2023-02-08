3ab91fd135cebdb38df34d5a1905340a2735058a
AyrtonPereira <ayrton_pereira@live.com>
2023-02-07 22:19:05 -0300

    chore: changed api url

--- src/api/Api.js
+++ src/api/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class API {
   constructor(options = {}) {
     this.api = axios.create({
-      baseURL: "http://localhost:3003",
+      baseURL: "https://json-server-ashy.vercel.app/",
       timeout: 3000,
       ...options,
     });
